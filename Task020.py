2952edd412c6355bb898d5ddaf36f39278cc3e37
Siarhei Davidzenka <mc.sergey30@gmail.com>
2023-07-06 05:11:11 +0200

    020

--- Task020.py
+++ Task020.py
@@ -7,8 +7,35 @@
 # что на вход подается только одно слово, которое содержит либо только английские, либо только русские буквы.
 
 # *Пример:*
-
 # ноутбук
 #     12
+    
+def check_language():    
+    cost_ru = {1:'АВЕИНОРСТ', 2:'ДКЛМПУ', 3:'БГЁЬЯ', 4:'ЙЫ', 5:'ЖЗХЦЧ', 8:'ШЭЮ', 10:'ФЩЪ'}
+    cost_en = {1:'AEIOULNSTR', 2:'DGBC', 3:'BCMP', 4:'FHVWY', 5:'K', 8:'JX', 10:'QZ'}
+    for (k, v) in cost_ru.items():
+        if word[0] in v:
+            return cost_ru
+        else:
+            return cost_en
 
-int(ф) = 0
+def get_key(dic, word):
+    sum = 0
+    for i in range(len(word)):
+        for (k, v) in dic.items():
+            if word[i] in v:
+                sum += k
+    print(f'The cost of the word entered by the usersum = {sum}.', end=' ')
+
+def task():
+    word = str(input('Please enter word: '))
+    word = word.upper()
+    dic = check_language()
+    get_key(dic, word)
+
+
+
+
+
+
+
