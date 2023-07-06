828f6807d7b57b2437c54deb36bfc36b469d18a7
Siarhei Davidzenka <mc.sergey30@gmail.com>
2023-07-06 05:14:11 +0200

    020_1

--- Task020.py
+++ Task020.py
@@ -10,7 +10,7 @@
 # ноутбук
 #     12
     
-def check_language():    
+def check_language(word):    
     cost_ru = {1:'АВЕИНОРСТ', 2:'ДКЛМПУ', 3:'БГЁЬЯ', 4:'ЙЫ', 5:'ЖЗХЦЧ', 8:'ШЭЮ', 10:'ФЩЪ'}
     cost_en = {1:'AEIOULNSTR', 2:'DGBC', 3:'BCMP', 4:'FHVWY', 5:'K', 8:'JX', 10:'QZ'}
     for (k, v) in cost_ru.items():
